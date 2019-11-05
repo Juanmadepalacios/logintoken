eff665b4e45d3f9cfe69630a79f07646b8b6a813
Juan Manuel de Palacios <juanma36@gmail.com>
2019-11-05 20:06:14 +0000

    login

--- src/main.py
+++ src/main.py
@@ -6,17 +6,31 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, create_refresh_token,
+    get_jwt_identity
+)
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import User
+
+
+from passlib.hash import pbkdf2_sha256 as sha256
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
+
+jwt = JWTManager(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -29,13 +43,49 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/hello', methods=['POST', 'GET'])
+@jwt_required
 def handle_hello():
-
+    current_user = get_jwt_identity()
     response_body = {
-        "hello": "world"
+        "hello": current_user
     }
 
     return jsonify(response_body), 200
+
+@app.route('/login', methods=['POST'])
+def handle_login():
+    data = request.json
+    user = User.query.filter_by(username = data["username"]).first()
+    if user is None:
+        return jsonify ({
+            "error": "el usuario no existe"
+        }), 404
+    if sha256.verify(data["password"], user.password):
+
+        mivariable = create_access_token(identity=data["username"])
+        refresh = create_refresh_token(identity=data["username"])
+        return jsonify ({
+            "token": mivariable,
+            "refresh": refresh
+        }), 200
+
+    return jsonify ({
+            "error":"la contraseña no es valida"
+    }), 404
+
+@app.route('/register', methods=['POST'])
+def handle_register():
+    data = request.json
+
+    user = User()
+    user.username = data["username"]
+    user.mail = data["mail"]
+    user.password = sha256.hash(data["password"])
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    username = db.Column(db.String(20), nullable =False)
+    password = db.Column(db.String(255), nullable =False)
+    mail = db.Column(db.String(20), nullable = False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username, self.password, self.mail
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "username": self.username,
+            "password": self.password,
+            "mail": self.mail,
+        }
+
